829a3ff93b4ff8588f1737c87027e784f69e856f
marcolehar35 <lehartel.com@gmail.com>
2020-01-23 11:12:38 +0100

    structure de base et qq styles

--- js/app.js
+++ js/app.js
@@ -2,8 +2,22 @@
  * Todolist
  */
 var app = {
-  init: function() {
-
+  init: function () {
+    // Génération du Form
+    app.createForm();
+    // Génération du Counter
+    app.createCounter();
+    // Génération de la List
+    app.createList();
+  },
+  createForm: function () {
+    console.log('app : createForm');
+  },
+  createCounter: function () {
+    console.log('app : createCounter');
+  },
+  createList: function () {
+    console.log('app : createList');
   },
 };
 
