e018eee33624382fc2d7d61177393f010742b27d
marcolehar35 <lehartel.com@gmail.com>
2020-01-23 11:38:33 +0100

    form ok

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 /**
  * Todolist
  */
-var app = {
+const app = {
   init: function () {
+    // Target du DOM
+    app.todo = document.getElementById('todo');
     // Génération du Form
     app.createForm();
     // Génération du Counter
@@ -12,6 +14,21 @@ var app = {
   },
   createForm: function () {
     console.log('app : createForm');
+    // ajout d'un <form>
+    const form = document.createElement('form');
+    form.id = 'todo-form';
+
+    // ajout d'un input text et l'ajouter au form
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.id = 'todo-input';
+    input.autocomplete = 'off';
+    input.name = 'inputValue';
+    input.placeholder = 'Ajouter une tâche';
+
+    // ajout du form dans le dom
+    form.appendChild(input);
+    app.todo.appendChild(form);
   },
   createCounter: function () {
     console.log('app : createCounter');
